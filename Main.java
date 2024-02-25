00ec8345db55e776f8b66902e3d043a1878f3105
darshan <darshankrishna96.dg96@gmail.com>
2024-02-25 15:19:22 -0800

    Added Main Java class

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("This is Main Program");
+    System.out.println("Ive written programm in Separate File for testing");
+  
+  }
+}
