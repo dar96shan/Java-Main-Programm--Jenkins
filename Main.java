a53bc4b6e33289733f10525221f82ef19d92ba37
darshan <darshankrishna96.dg96@gmail.com>
2024-02-25 15:54:47 -0800

    Added new file Student and called in Main

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import java.util.Date;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
     System.out.println("This is Main Program");
     System.out.println("Ive written programm in Separate File for testing");
     System.out.println(new Date());
     System.out.println("Added one more file ");
+
+    Student stu = new Student();
+    stu.sayHello();
+  
   }
 }
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student{
+
+  public Student(){
+    System.out.println("Creating Student Constructor");
+  }
+
+  public void sayHello(){
+    System.out.println("Hi i am Student of Jenkin class");
+  }
+}
