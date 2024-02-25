355f143295075800e01d6ffb8a05723a74c124dc
darshan <darshankrishna96.dg96@gmail.com>
2024-02-25 15:23:47 -0800

    Added new Date line Main class

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Date;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("This is Main Program");
     System.out.println("Ive written programm in Separate File for testing");
+    System.out.println(new Date());
   
   }
 }
